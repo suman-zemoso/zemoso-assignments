71454d5cbaf57a1385c06219d2ae7b0384662fab
suman-zemoso <saisuman.kopparthi@zemosolabs.com>
2017-06-20 14:45:17 +0530

    assignment 2 completed

---
+++ java assignment 2/AllLetters.java
@@ -0,0 +1,26 @@
+import java.util.regex.*;
+import java.util.*;
+
+class AlphabetCheck {
+	final Pattern p = Pattern.compile("[a-z]", Pattern.CASE_INSENSITIVE);
+	Matcher m;
+	Set<String> set = new HashSet<String>();
+	String removeAlphabet(String s) {
+		m = p.matcher(s);
+		while(m.find()) {
+			set.add(m.group().toUpperCase());
+			s = s.replaceAll(m.group(), "");
+		}
+		return s;
+	}
+}
+public class AllLetters {
+	public static void main(String... args) {
+		AlphabetCheck ac = new AlphabetCheck();
+		System.out.print("enter the string: ");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		s = ac.removeAlphabet(s);
+		System.out.println(ac.set.size() == 26?"the string contains all the alphabets":"the string doesn't contain all the strings");
+	}
+}
