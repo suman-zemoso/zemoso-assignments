5bd66b13e8db9e4efc7f419491ee93633e505576
suman-zemoso <saisuman.kopparthi@zemosolabs.com>
2017-07-13 19:38:52 +0530

    after changes

--- DBMS/Projects and Employees/employee_project.sql
+++ DBMS/Projects and Employees/employee_project.sql
@@ -1,16 +1,17 @@
-
-CREATE SCHEMA IF NOT EXISTS `employee_project` ;
-USE `employee_project` ;
+CREATE SCHEMA IF NOT EXISTS `mydb`;
+USE `mydb` ;
 
 CREATE TABLE IF NOT EXISTS `employee` (
   `employee-id` INT NOT NULL,
   `name` VARCHAR(20) NOT NULL,
   `gender` ENUM('male', 'female') NULL,
+  `age` INT NULL,
+  `experience` INT NULL,
   `job-title` VARCHAR(20) NULL,
   `created-by` VARCHAR(20) NOT NULL,
-  `created-at` DATETIME NULL,
+  `created-at` DATETIME NOT NULL,
   `modified-by` VARCHAR(20) NOT NULL,
-  `modified-at` DATETIME NULL,
+  `modified-at` DATETIME NOT NULL,
   PRIMARY KEY (`employee-id`));
 
 
@@ -34,18 +35,36 @@ CREATE TABLE IF NOT EXISTS `report_to` (
     ON UPDATE CASCADE);
 
 
-CREATE TABLE IF NOT EXISTS `part_of_project` (
+CREATE TABLE IF NOT EXISTS `project` (
+  `project-id` INT NOT NULL,
+  `project-name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(60) NULL,
+  `status` ENUM('completed', 'not completed') NOT NULL,
+  `budget` INT NULL,
+  `deadline` DATETIME NOT NULL,
+  `created-by` VARCHAR(45) NOT NULL,
+  `created-at` DATETIME NOT NULL,
+  `modified-by` VARCHAR(45) NOT NULL,
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`project-id`));
+
+
+CREATE TABLE IF NOT EXISTS `part_of` (
   `employee-id` INT NOT NULL,
   `project-id` INT NOT NULL,
-  `project-name` VARCHAR(20) NOT NULL,
   `created-by` VARCHAR(20) NOT NULL,
-  `created-at` DATETIME NULL,
+  `created-at` DATETIME NOT NULL,
   `modified-by` VARCHAR(20) NOT NULL,
-  `modified-at` DATETIME NULL,
-  PRIMARY KEY (`employee-id`),
-  CONSTRAINT `fk_part_of_project_1`
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`employee-id`, `project-id`),
+  CONSTRAINT `fk_part_of_1`
     FOREIGN KEY (`employee-id`)
     REFERENCES `employee` (`employee-id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_part_of_2`
+    FOREIGN KEY (`project-id`)
+    REFERENCES `project` (`project-id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE);
 
