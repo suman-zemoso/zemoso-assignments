d63ff85d2f16f90b9637c4864f382502576f1268
suman-zemoso <saisuman.kopparthi@zemosolabs.com>
2017-07-14 17:52:03 +0530

    IMDB completed

---
+++ DBMS/IMDB/IMDB.sql
@@ -0,0 +1,85 @@
+
+CREATE SCHEMA IF NOT EXISTS `IMDB` ;
+USE `IMDB` ;
+
+CREATE TABLE IF NOT EXISTS `IMDB`.`actor` (
+  `actor-id` INT NOT NULL,
+  `first-name` VARCHAR(20) NOT NULL,
+  `last-name` VARCHAR(20) NULL,
+  `age` INT NOT NULL,
+  `gender` ENUM('male', 'female') NULL,
+  `experience` INT NULL,
+  `created-by` VARCHAR(40) NOT NULL,
+  `created-at` DATETIME NOT NULL,
+  `modified-by` VARCHAR(40) NOT NULL,
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`actor-id`));
+
+
+CREATE TABLE IF NOT EXISTS `IMDB`.`movie` (
+  `movie-id` INT NOT NULL,
+  `name` VARCHAR(30) NOT NULL,
+  `rating` INT NULL,
+  `genre` VARCHAR(45) NOT NULL,
+  `budget` INT NULL,
+  `release-year` INT NOT NULL,
+  `created-by` VARCHAR(40) NOT NULL,
+  `created-at` DATETIME NOT NULL,
+  `modified-by` VARCHAR(40) NOT NULL,
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`movie-id`));
+
+
+CREATE TABLE IF NOT EXISTS `IMDB`.`tv_series` (
+  `tv_series-id` INT NOT NULL,
+  `name` VARCHAR(30) NOT NULL,
+  `genre` VARCHAR(45) NULL,
+  `status` ENUM('running', 'stopped') NULL,
+  `created-by` VARCHAR(40) NOT NULL,
+  `created-at` DATETIME NOT NULL,
+  `modified-by` VARCHAR(40) NOT NULL,
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`tv_series-id`));
+
+
+CREATE TABLE IF NOT EXISTS `IMDB`.`actor_movie` (
+  `movie-id` INT NOT NULL,
+  `actor-id` INT NOT NULL,
+  `created-by` VARCHAR(40) NOT NULL,
+  `created-at` DATETIME NOT NULL,
+  `modified-by` VARCHAR(40) NOT NULL,
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`movie-id`, `actor-id`),
+  CONSTRAINT `fk_acts_in_movie_1`
+    FOREIGN KEY (`movie-id`)
+    REFERENCES `IMDB`.`movie` (`movie-id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_acts_in_movie_2`
+    FOREIGN KEY (`actor-id`)
+    REFERENCES `IMDB`.`actor` (`actor-id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE);
+
+
+CREATE TABLE IF NOT EXISTS `IMDB`.`actor_tv_series` (
+  `actor-id` INT NOT NULL,
+  `tv_series-id` INT NOT NULL,
+  `created-by` VARCHAR(40) NOT NULL,
+  `created-at` DATETIME NOT NULL,
+  `modified-by` VARCHAR(40) NOT NULL,
+  `modified-at` DATETIME NOT NULL,
+  PRIMARY KEY (`actor-id`, `tv_series-id`),
+  CONSTRAINT `fk_acts_in_tv_series_1`
+    FOREIGN KEY (`tv_series-id`)
+    REFERENCES `IMDB`.`tv_series` (`tv_series-id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_acts_in_tv_series_2`
+    FOREIGN KEY (`actor-id`)
+    REFERENCES `IMDB`.`actor` (`actor-id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE);
+
+
+
