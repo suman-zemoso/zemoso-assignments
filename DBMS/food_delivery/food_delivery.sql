9f0ca3147e9749a86bdd71cbb11e8bf1c02b3467
suman-zemoso <saisuman.kopparthi@zemosolabs.com>
2017-07-17 16:06:21 +0530

    combined weak entity and relationship table

--- DBMS/food_delivery/food_delivery.sql
+++ DBMS/food_delivery/food_delivery.sql
@@ -1,8 +1,8 @@
 
-CREATE SCHEMA IF NOT EXISTS `food_delivery`;
-USE `food_delivery` ;
+CREATE SCHEMA IF NOT EXISTS `food_delivery2` ;
+USE `food_delivery2` ;
 
-CREATE TABLE IF NOT EXISTS `food_delivery`.`customer` (
+CREATE TABLE IF NOT EXISTS `food_delivery2`.`customer` (
   `customer-id` INT NOT NULL,
   `first-name` VARCHAR(20) NOT NULL,
   `last-name` VARCHAR(20) NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `food_delivery`.`customer` (
   PRIMARY KEY (`customer-id`));
 
 
-CREATE TABLE IF NOT EXISTS `food_delivery`.`delivery_person` (
+CREATE TABLE IF NOT EXISTS `food_delivery2`.`delivery_person` (
   `delivery_person-id` INT NOT NULL,
   `first-name` VARCHAR(20) NOT NULL,
   `last-name` VARCHAR(20) NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `food_delivery`.`delivery_person` (
   PRIMARY KEY (`delivery_person-id`));
 
 
-CREATE TABLE IF NOT EXISTS `food_delivery`.`restaurant` (
+CREATE TABLE IF NOT EXISTS `food_delivery2`.`restaurant` (
   `restaurant-id` INT NOT NULL,
   `name` VARCHAR(30) NOT NULL,
   `cousine` VARCHAR(45) NULL,
@@ -41,58 +41,32 @@ CREATE TABLE IF NOT EXISTS `food_delivery`.`restaurant` (
   PRIMARY KEY (`restaurant-id`));
 
 
-CREATE TABLE IF NOT EXISTS `food_delivery`.`order` (
+CREATE TABLE IF NOT EXISTS `food_delivery2`.`food-order` (
   `order-id` INT NOT NULL,
-  `items` VARCHAR(45) NULL,
-  `created-by` VARCHAR(40) NOT NULL,
-  `created-at` DATETIME NOT NULL,
-  `modified-by` VARCHAR(40) NOT NULL,
-  `modified-at` DATETIME NOT NULL,
-  PRIMARY KEY (`order-id`));
-
-
-CREATE TABLE IF NOT EXISTS `food_delivery`.`customer_restaurant_order` (
   `customer-id` INT NOT NULL,
   `restaurant-id` INT NOT NULL,
-  `order-id` INT NOT NULL,
-  `created-by` VARCHAR(40) NOT NULL,
-  `created-at` DATETIME NOT NULL,
-  `modified-by` VARCHAR(40) NOT NULL,
-  `modified-at` DATETIME NOT NULL,
-  PRIMARY KEY (`customer-id`, `restaurant-id`, `order-id`),
-  CONSTRAINT `fk_customer_restaurant_order_1`
-    FOREIGN KEY (`customer-id`)
-    REFERENCES `food_delivery`.`customer` (`customer-id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_customer_restaurant_order_2`
-    FOREIGN KEY (`restaurant-id`)
-    REFERENCES `food_delivery`.`restaurant` (`restaurant-id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_customer_restaurant_order_3`
-    FOREIGN KEY (`order-id`)
-    REFERENCES `food_delivery`.`order` (`order-id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE);
-
-
-CREATE TABLE IF NOT EXISTS `food_delivery`.`delivery_person_order` (
-  `order-id` INT NOT NULL,
   `delivery_person-id` INT NOT NULL,
+  `items` VARCHAR(45) NULL,
+  `date-of-booking` DATE NOT NULL,
   `created-by` VARCHAR(40) NOT NULL,
   `created-at` DATETIME NOT NULL,
   `modified-by` VARCHAR(40) NOT NULL,
   `modified-at` DATETIME NOT NULL,
-  PRIMARY KEY (`order-id`, `delivery_person-id`),
-  CONSTRAINT `fk_delivered_by_1`
-    FOREIGN KEY (`order-id`)
-    REFERENCES `food_delivery`.`order` (`order-id`)
+  PRIMARY KEY (`order-id`),
+  UNIQUE (`customer-id`, `date-of-booking`),
+  CONSTRAINT `fk_order_1`
+    FOREIGN KEY (`customer-id`)
+    REFERENCES `food_delivery2`.`customer` (`customer-id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_delivered_by_2`
+  CONSTRAINT `fk_order_2`
+    FOREIGN KEY (`restaurant-id`)
+    REFERENCES `food_delivery2`.`restaurant` (`restaurant-id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_order_3`
     FOREIGN KEY (`delivery_person-id`)
-    REFERENCES `food_delivery`.`delivery_person` (`delivery_person-id`)
+    REFERENCES `food_delivery2`.`delivery_person` (`delivery_person-id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE);
 
